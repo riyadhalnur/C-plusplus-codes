e0be4a29f15b9ddcd79ee20d5a6bdd9d4bbf7dd6
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2015-10-24 22:12:13 +0600

    Uncomment 2 code files

--- Exception_Handling/example7a.cpp
+++ Exception_Handling/example7a.cpp
@@ -1,18 +1,22 @@
-/*#include<iostream>
+#include<iostream>
 #include<exception>
 using namespace std;
+
 class myException : public exception{
 public:
 	myException(char *s) : exception(s){}
 };
+
 int main(){
 	int m = 10 , n = 0;
-    try{		
+
+  try {
 		if( n == 0 ) throw myException("Division by zero..");
 		else if( m < n ) throw myException("Dividend is lesser..");
 		cout << "Result: " << m/n << endl;
-    }catch(myException &e){
-        cout << e.what() << endl;
-    }    
-    return 0;
-}*/
+	} catch(myException &e) {
+		cout << e.what() << endl;
+  }
+
+  return 0;
+}
--- Exception_Handling/example7b.cpp
+++ Exception_Handling/example7b.cpp
@@ -1,23 +1,27 @@
-/*#include<iostream>
+#include<iostream>
 #include<exception>
 using namespace std;
+
 class myException : public exception{
 	char *e;
 public:
 	myException(char *s){ e = s; }
-	const char* what(){ 
+	const char* what(){
 		cout << "overriddern in myException class.." << endl;
-		return e; 
+		return e;
 	}
 };
+
 int main(){
 	int m = 10 , n = 0;
-    try{		
-		if( n == 0 ) throw myException("Division by zero..");
+
+  try {
+		if ( n == 0 ) throw myException("Division by zero..");
 		else if( m < n ) throw myException("Dividend is lesser..");
 		cout << "Result: " << m/n << endl;
-    }catch(myException &e){
-        cout << e.what() << endl;
-    }    
-    return 0;
-}*/
+	} catch (myException &e) {
+		cout << e.what() << endl;
+	}
+
+	return 0;
+}
